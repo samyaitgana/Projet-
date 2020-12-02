5d65891ef1e769104b304a0cd79ebd0689d3dcaa
samyaitgana <75205449+samyaitgana@users.noreply.github.com>
2020-12-02 22:53:47 +0100

    Delete translation.h

--- translation.h
+++
@@ -1,9 +0,0 @@
-#ifndef __H_TRANSLATION
-#define __H_TRANSLATION
-
-#include "tree.h"
-
-void compress(Dictionary *dict, char *string, char **compressed, long unsigned int prefix);
-void decompress(Dictionary *dict, char *compressed, char **string, long unsigned int prefix);
-
-#endif
